24f1651090721a24b02acd6afc2bcf93642fba2c
erika.duarte <erika.duarte.mora@gmail.com>
2024-07-09 11:21:05 -0500

    todo lo de paciente y security

--- api/src/main/java/med/voll/api/repository/MedicoRepository.java
+++
@@ -1,7 +0,0 @@
-package med.voll.api.repository;
-
-import med.voll.api.models.Medico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicoRepository extends JpaRepository<Medico, Long> {
-}
